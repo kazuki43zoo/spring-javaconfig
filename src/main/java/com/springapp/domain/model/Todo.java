944fa47c7fab91ba0b9b4548f178bbd17d5cbdd3
Kazuki Shimizu <kazuki43zoo@gmail.com>
2014-11-14 04:21:38 +0900

    disable lombok.

--- src/main/java/com/springapp/domain/model/Todo.java
+++ src/main/java/com/springapp/domain/model/Todo.java
@@ -1,13 +1,43 @@
 package com.springapp.domain.model;
 
-import lombok.Data;
 
 import java.util.Date;
 
-@Data
 public class Todo {
     private String todoId;
     private String todoTitle;
     private Boolean finished;
     private Date createdAt;
+
+    public String getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoTitle() {
+        return todoTitle;
+    }
+
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
+    }
+
+    public Boolean getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Boolean finished) {
+        this.finished = finished;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
